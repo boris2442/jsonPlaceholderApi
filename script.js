e6bbc429277427ebfd6979032f821a3e97294390
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-10 10:46:33 +0100

    text

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+// // const fetchUser=as()=>{
+//     function json(){
+
+//         fetch("https://jsonplaceholder.typicode.com/users").then((resp)=>resp.json())
+//         .then(user=>{
+//         console.log(user.id)
+//         document.body.innerHTML =`
+//         <h1>id: ${user.id}</h1>
+//         <p>name: ${user.name}</p>
+//         <p>email: ${user.email}</p>
+//         <p>adress: ${user.address.city}</p>  
+//         <p>phone:${user.phone}</p>
+//         <p>geolocalisation:latitude:${user.address.geo.lat}</p>
+//         <p>geolocalisation:longitude:${user.address.geo.lng}</p>
+//         <p>website:${user.website}</p>
+//         `
+//         })
+//     }
+// // }
+// document.querySelector("button").addEventListener("click", json)
+console.log("text")
