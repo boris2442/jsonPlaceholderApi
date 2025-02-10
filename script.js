58364c7753119d53bb5259cdd8cae6da7178561a
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-10 18:44:13 +0100

    codage

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+const fetchUser = async () => {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await response.json();
+    let container = document.querySelector(".user-container");
+    container.innerHTML = "";
 
+    users.forEach(user => {
+        let div = document.createElement("div");
+        div.classList.add("user-card")
+        div.innerHTML = `
+<h1>id: ${user.id}</h1>
+<p>name: ${user.name}</p>
+<p>email: ${user.email}</p>
+<p>adress: ${user.address.city}</p>
+<p>phone:${user.phone}</p>
+<p>geolocalisation:latitude:${user.address.geo.lat}</p>
+<p>geolocalisation:longitude:${user.address.geo.lng}</p>
+<p>website:${user.website}</p>`
+        container.appendChild(div)
 
-// // const fetchUser=as()=>{
-//     function json(){
+    })
 
-//         fetch("https://jsonplaceholder.typicode.com/users").then((resp)=>resp.json())
-//         .then(user=>{
-//         console.log(user.id)
-//         document.body.innerHTML =`
-//         <h1>id: ${user.id}</h1>
-//         <p>name: ${user.name}</p>
-//         <p>email: ${user.email}</p>
-//         <p>adress: ${user.address.city}</p>  
-//         <p>phone:${user.phone}</p>
-//         <p>geolocalisation:latitude:${user.address.geo.lat}</p>
-//         <p>geolocalisation:longitude:${user.address.geo.lng}</p>
-//         <p>website:${user.website}</p>
-//         `
-//         })
-//     }
-// // }
-// document.querySelector("button").addEventListener("click", json)
-console.log("text")
+}
+fetchUser()
